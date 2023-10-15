7a54352958d05a3294328fc9c12db2f2eba23277
ArtDev <89361400+ArtRiv@users.noreply.github.com>
2023-10-15 19:24:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+const minutesSeconds = document.querySelector("#minutesSeconds");
+const minutesSecondsInput = document.querySelector("#minutesSecondsInput");
+
+const inputTimeSeconds = document.querySelector("#inputTimeSeconds");
+const inputTimeMinutes = document.querySelector("#inputTimeMinutes");
+const minutesInputLeftZero = document.querySelector("#minutesInputLeftZero");
+const secondsInputLeftZero = document.querySelector("#secondsInputLeftZero");
+const spanInput = document.querySelector("#spanInput");
+
+const secondsTimer = document.querySelector("#secondsTimer");
+const minutesTimer = document.querySelector("#minutesTimer");
+
+const startButton = document.querySelector("#startButton");
+
+inputTimeMinutes.addEventListener("input", function() {
+    inputTimeMinutes.value = inputTimeMinutes.value.replace(/[^0-9]/g, '');
+});
+
+inputTimeSeconds.addEventListener("input", function() {
+
+    inputTimeSeconds.value = inputTimeSeconds.value.replace(/[^0-9]/g, '');
+});
+
+minutesSeconds.addEventListener("click", function() {
+    minutesSeconds.style.display = "none";
+    minutesSecondsInput.style.display = "flex";
+}); 
+
+startButton.addEventListener("click", function() {
+    let seconds = parseInt(inputTimeSeconds.value);
+    let minutes = parseInt(inputTimeMinutes.value);
+
+    if (seconds > 59) {
+        seconds = 59;
+        console.log(seconds);
+    }
+    if (minutes < 10) {
+        minutesInputLeftZero.style.display = "flex";
+    }
+
+    function mainTimerSecondsFunction() { 
+
+        if (seconds < 10 && seconds > 0){
+            secondsInputLeftZero.style.display = "flex";
+
+            seconds--;
+            console.log(seconds);
+
+            minutesSeconds.style.display = "flex";
+            minutesSecondsInput.style.display = "none";
+
+            secondsTimer.innerHTML = seconds;
+            minutesTimer.innerHTML = minutes;
+        }
+        else if (seconds == 10) {
+            seconds--;
+            console.log(seconds);
+            
+            minutesSeconds.style.display = "flex";
+            minutesSecondsInput.style.display = "none";
+            
+            secondsTimer.innerHTML = seconds;
+            minutesTimer.innerHTML = minutes;
+
+            secondsInputLeftZero.style.display = "flex";
+        }
+        else if (seconds > 10) {
+            seconds--;
+            console.log(seconds);
+            
+            minutesSeconds.style.display = "flex";
+            minutesSecondsInput.style.display = "none";
+
+            secondsTimer.innerHTML = seconds;
+            minutesTimer.innerHTML = minutes;
+        }
+        else {
+            mainTimerMinutesFunction();
+        }
+    };
+
+    function mainTimerMinutesFunction() {
+        if (minutes > 0) {
+            seconds = 59;
+            minutes--;
+            console.log(minutes);
+
+            minutesTimer.innerHTML = minutes;
+            secondsTimer.innerHTML = seconds;
+            secondsInputLeftZero.style.display = "none";
+        }
+    }
+
+    const intervalId = setInterval(mainTimerSecondsFunction, 1000);
+
+});
